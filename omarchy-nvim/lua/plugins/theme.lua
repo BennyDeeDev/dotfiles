d74eb0449af0d1dc695200795a2475eada9ac8b6
Benjamin Derksen <benjamin.derksen.98@gmail.com>
2025-08-23 17:19:13 +0200

    introduce omarchy

---
+++ omarchy-nvim/lua/plugins/theme.lua
@@ -0,0 +1,1 @@
+/home/benny/.config/omarchy/current/theme/neovim.lua
---
+++ symlinks-omarchy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DOTFILES="$HOME/Repos/dotfiles"
+
+ln -sfn "$DOTFILES/omarchy-hypr" ~/.config/hypr
+ln -sfn "$DOTFILES/omarchy-waybar" ~/.config/waybar
+ln -sfn "$DOTFILES/omarchy-nvim" ~/.config/nvim
