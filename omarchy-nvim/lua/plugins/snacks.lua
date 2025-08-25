a7854d78787c3fca84fe56e6fd54bc5e00b1d289
Benjamin Derksen <benjamin.derksen.98@gmail.com>
2025-08-25 21:42:35 +0200

    remove neotree and show hidden files

--- omarchy-nvim/lua/plugins/snacks.lua
+++ omarchy-nvim/lua/plugins/snacks.lua
@@ -1,8 +1,13 @@
 return {
   "folke/snacks.nvim",
   opts = {
-    scroll = {
-      enabled = true,
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = true,
+        },
+      },
     },
   },
 }
