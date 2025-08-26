bcadd69f7cf9b1474bb140729212e313b3c3fb92
Benjamin Derksen <benjamin.derksen.98@gmail.com>
2025-08-26 12:46:50 +0200

    share common bash functions, needs to be tested on other systems later

--- symlinks-omarchy.sh
+++ symlinks-omarchy.sh
@@ -5,3 +5,4 @@ DOTFILES="$HOME/Repos/dotfiles"
 ln -sfn "$DOTFILES/omarchy-hypr" ~/.config/hypr
 ln -sfn "$DOTFILES/omarchy-waybar" ~/.config/waybar
 ln -sfn "$DOTFILES/omarchy-nvim" ~/.config/nvim
+ln -sfn "$DOTFILES/.bashrc.omarchy" ~/.bashrc
