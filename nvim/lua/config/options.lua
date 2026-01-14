b55b082e40ff949fe9066cbaafd3cb3db8239826
BennyDeeDev <45900418+BennyDeeDev@users.noreply.github.com>
2026-01-14 15:20:32 +0100

    update lazyvim config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,6 +3,6 @@
 -- Add any additional options here
 
 vim.opt.scrolloff = 16
-
+vim.opt.relativenumber = false
 vim.o.title = true
 vim.o.titlestring = "%t – " .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t") .. " – nvim"
