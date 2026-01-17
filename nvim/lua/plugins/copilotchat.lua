c8f73d7887a5a3d12313cf0b420e655e182c527b
BennyDeeDev <45900418+BennyDeeDev@users.noreply.github.com>
2026-01-17 19:45:39 +0100

    typo

--- nvim/lua/plugins/copilotchat.lua
+++ nvim/lua/plugins/copilotchat.lua
@@ -2,7 +2,7 @@ return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     opts = function(_, opts)
-      opts.model = "gpt5.2"
+      opts.model = "gpt-5.2"
       opts.sticky = { "#buffer:listed" }
       return opts
     end,
