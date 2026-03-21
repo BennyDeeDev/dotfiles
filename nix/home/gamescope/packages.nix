62f2d812db29bb7f042f2e3e68080110c7e0049e
BennyDeeDev <45900418+BennyDeeDev@users.noreply.github.com>
2026-03-21 22:00:51 +0100

    nix: properly propagate env vars

--- nix/home/gamescope/packages.nix
+++ nix/home/gamescope/packages.nix
@@ -7,20 +7,6 @@
     wqy_zenhei
   ];
 
-  xdg.desktopEntries.steam = {
-    name = "Steam";
-    exec = "env GDK_SCALE=2 steam";
-    icon = "steam";
-    terminal = false;
-    type = "Application";
-    categories = [ "Network" "FileTransfer" "Game" ];
-    mimeType = [ "x-scheme-handler/steam" "x-scheme-handler/steamlink" ];
-    settings = {
-      PrefersNonDefaultGPU = "true";
-      X-KDE-RunOnDiscreteGpu = "true";
-    };
-  };
-
   services.flatpak.update.auto = {
     enable = true;
     onCalendar = "weekly";
