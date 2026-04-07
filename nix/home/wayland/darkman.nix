7f37300b9913b0f3d14fd92b0f02f29f76e96303
BennyDeeDev <45900418+BennyDeeDev@users.noreply.github.com>
2026-04-07 20:41:27 +0200

    fix darkman but fr this time

--- nix/home/wayland/darkman.nix
+++ nix/home/wayland/darkman.nix
@@ -8,12 +8,20 @@
       lng = 7.468429;
     };
     lightModeScripts.omarchy = ''
-      /run/current-system/sw/bin/bash $HOME/.local/share/omarchy/bin/omarchy-theme-set catppuccin-latte-extended
-      /etc/profiles/per-user/$USER/bin/asdbctl set 40
+      export PATH="/run/current-system/sw/bin:/etc/profiles/per-user/$USER/bin:$HOME/.local/share/omarchy/bin"
+      export OMARCHY_PATH="$HOME/.local/share/omarchy"
+      export WAYLAND_DISPLAY=$(systemctl --user show-environment | grep '^WAYLAND_DISPLAY=' | cut -d= -f2)
+      export HYPRLAND_INSTANCE_SIGNATURE=$(systemctl --user show-environment | grep '^HYPRLAND_INSTANCE_SIGNATURE=' | cut -d= -f2)
+      omarchy-theme-set catppuccin-latte-extended
+      asdbctl set 40
     '';
     darkModeScripts.omarchy = ''
-      /run/current-system/sw/bin/bash $HOME/.local/share/omarchy/bin/omarchy-theme-set catppuccin-extended
-      /etc/profiles/per-user/$USER/bin/asdbctl set 20
+      export PATH="/run/current-system/sw/bin:/etc/profiles/per-user/$USER/bin:$HOME/.local/share/omarchy/bin"
+      export OMARCHY_PATH="$HOME/.local/share/omarchy"
+      export WAYLAND_DISPLAY=$(systemctl --user show-environment | grep '^WAYLAND_DISPLAY=' | cut -d= -f2)
+      export HYPRLAND_INSTANCE_SIGNATURE=$(systemctl --user show-environment | grep '^HYPRLAND_INSTANCE_SIGNATURE=' | cut -d= -f2)
+      omarchy-theme-set catppuccin-extended
+      asdbctl set 20
     '';
   };
 }
