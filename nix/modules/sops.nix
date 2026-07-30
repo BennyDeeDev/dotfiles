b5e3b1506f46f4b153e275338afd9d8182dbacad
BennyDeeDev <45900418+BennyDeeDev@users.noreply.github.com>
2026-07-30 18:42:48 +0200

    reenroll pi5-server and ensure sudo password is reconciled with mutable users false

--- nix/modules/sops.nix
+++ nix/modules/sops.nix
@@ -1,5 +1,8 @@
 { ... }:
 {
+  # Without this, hashedPasswordFile is only seeded at user creation
+  users.mutableUsers = false;
+
   sops.age.keyFile = "/var/lib/sops-nix/key.txt";
   sops.age.generateKey = true;
   sops.age.sshKeyPaths = [ ];
